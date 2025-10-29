3a68ecf0a946ca759fccc8cd97368bb314c7dbd4
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2025-10-29 14:36:37 +0600

    updaer

--- src/app/modules/powerbi/powerbi.controller.ts
+++ src/app/modules/powerbi/powerbi.controller.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
-import {  PowerBIService } from "./powerbi.service";
-
+import { PowerBIService } from "./powerbi.service";
+import axios from "axios";
 
 export const PowerBIController = {
   async getReports(req: Request, res: Response) {
     try {
+
+
       const workspaceId = process.env.WORKSPACE_ID!;
       const reports = await PowerBIService.getReports(workspaceId);
       res.json({ success: true, data: reports });
